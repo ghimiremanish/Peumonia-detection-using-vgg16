9bdf5ff7f5a617ed375d527429f620504d558abc
Algoriithm <algoriithm@Algorithm.local>
2022-09-08 01:49:56 +0545

    dependency issue fixed

--- app/views.py
+++ app/views.py
@@ -1,26 +1,25 @@
 from flask import render_template, request, jsonify
 from app import app
 import os
-# import numpy as np
+import numpy as np
 import datetime
-# from keras.models import load_model
-# from keras.preprocessing import image
-# from PIL import Image
-# from werkzeug.utils import secure_filename
+from keras.models import load_model
+from keras.preprocessing import image
+from PIL import Image
+from werkzeug.utils import secure_filename
 
-# MODEL_PATH = 'models/model.h5'
+MODEL_PATH = 'app/models/model.h5'
 
 #Load your trained model
-# model = load_model(MODEL_PATH)
+model = load_model(MODEL_PATH)
 
-# def model_predict(img_path, model):
-#    img = image.load_img(img_path, target_size=(64, 64)) #target_size must agree with what the trained model expects!!
-
+def model_predict(img_path, model):
+   img = image.load_img(img_path, target_size=(64, 64)) #target_size must agree with what the trained model expects!!
    # Preprocessing the image
-   # img = image.img_to_array(img)
-   # img = np.expand_dims(img, axis=0)
-   # preds = model.predict(img)
-   # return preds
+   img = image.img_to_array(img)
+   img = np.expand_dims(img, axis=0)
+   preds = model.predict(img)
+   return preds
 
 @app.route('/')
 def home():
@@ -30,30 +29,30 @@ def home():
 def about():
    return render_template('about.html')
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
+@app.route('/predict', methods=['POST'])
+def predict():
    # Get the file from post request
-   # f = request.files['image']
+   f = request.files['image']
 
-   # # Save the file to ./uploads
-   # basepath = os.path.dirname(__file__)
-   # file_path = os.path.join(
-   #    basepath, 'uploads', secure_filename(str(datetime.datetime.today())+f.filename))
-   # f.save(file_path)
+   # Save the file to ./uploads
+   basepath = os.path.dirname(__file__)
+   file_path = os.path.join(
+      basepath, 'uploads', secure_filename(str(datetime.datetime.today())+f.filename))
+   f.save(file_path)
 
-   # # Make prediction
-   # # preds = model_predict(file_path, model)
+   # Make prediction
+   preds = model_predict(file_path, model)
 
-   # data = ''
+   data = ''
 
-   # if preds == 0:
-   #    data = 'Normal'
-   # else:
-   #    data = 'Pneumonia'
+   if preds == 0:
+      data = 'Normal'
+   else:
+      data = 'Pneumonia'
 
-   # tasks = [
-   #    {
-   #       'result':data
-   #    }
-   # ]
-   # return jsonify(tasks)
+   tasks = [
+      {
+         'result':data
+      }
+   ]
+   return jsonify(tasks)
